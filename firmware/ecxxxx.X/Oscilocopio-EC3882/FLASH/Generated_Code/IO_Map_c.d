49a60c52339ea60966bae156b92717444f110c33
mhurtado12 <12-10004@usb.ve>
2019-10-08 08:24:56 -0400

    Comunicacion serial funcionando
    Envia la trama de prueba y se ve en el realterm como se esperaba.

--- firmware/ecxxxx.X/Oscilocopio-EC3882/FLASH/Generated_Code/IO_Map_c.d
+++ firmware/ecxxxx.X/Oscilocopio-EC3882/FLASH/Generated_Code/IO_Map_c.d
@@ -1,6 +1,6 @@
 
 Generated_Code/IO_Map_c.obj: \
-	C:/Users/mhurtado/workspace/Oscilocopio-EC3882/Project_Headers/../Generated_Code/IO_Map.c \
-	C:/Users/mhurtado/workspace/Oscilocopio-EC3882/Generated_Code/PE_Types.h \
-	C:/Users/mhurtado/workspace/Oscilocopio-EC3882/Generated_Code/IO_Map.h 
+	C:/Users/ALE/Documents/GitHub/EC3883-G02/firmware/ecxxxx.X/Oscilocopio-EC3882/Project_Headers/../Generated_Code/IO_Map.c \
+	C:/Users/ALE/Documents/GitHub/EC3883-G02/firmware/ecxxxx.X/Oscilocopio-EC3882/Generated_Code/PE_Types.h \
+	C:/Users/ALE/Documents/GitHub/EC3883-G02/firmware/ecxxxx.X/Oscilocopio-EC3882/Generated_Code/IO_Map.h 
 
